10386695fa48dafe118c907fabc147ab32bbac5d
khoant <chelsea.edenhazard16@gmail.com>
2023-10-11 00:49:02 +0700

    commit

---
+++ CoreData/dbo/Tables/Article.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Article](
+	[Id] INT NOT NULL,
+	[ArticleName] [nvarchar](500) NOT NULL,
+	[Status] [nvarchar](255) NOT NULL,
+	[CreationDate] [datetime2](7) NOT NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
+	[RefURL] [nvarchar](255) NOT NULL,
+	[ImageThumb] [nvarchar](255) NULL,
+	[Description] [nvarchar](500) NULL,
+	[CategoryId] [int] NOT NULL,
+ CONSTRAINT [Pk_Article_Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Article]  WITH CHECK ADD  CONSTRAINT [Fk_Article_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([Id])
+GO
+
+ALTER TABLE [dbo].[Article] CHECK CONSTRAINT [Fk_Article_UserId]
+GO
---
+++ CoreData/dbo/Tables/ArticleContent.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[ArticleContent](
+	[Id] [uniqueidentifier] NOT NULL,
+	[ArticleId] INT UNIQUE NOT NULL,
+	[Content] [nvarchar](max) NULL,
+ CONSTRAINT [Pk_ArticleContent_Id] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ArticleContent]  WITH CHECK ADD  CONSTRAINT [Fk_ArticleContent_ArticleId] FOREIGN KEY([ArticleId])
+REFERENCES [dbo].[Article] ([Id])
+GO
+
+ALTER TABLE [dbo].[ArticleContent] CHECK CONSTRAINT [Fk_ArticleContent_ArticleId]
+GO
